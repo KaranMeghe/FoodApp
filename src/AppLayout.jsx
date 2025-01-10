5b0a5a8a62999cfe46541370e6a11f5e200e33d6
Karan Meghe <karanmeghe87@gmail.com>
2025-01-10 20:15:04 +0530

    add - created redux store & slices, store is connected with react app

--- src/AppLayout.jsx
+++ src/AppLayout.jsx
@@ -2,21 +2,25 @@
 import { Outlet } from "react-router";
 import { Footer, Header, NetworkMonitor } from "./components";
 import useOnlineStatus from "./hooks/useOnlineStatus";
+import { Provider } from "react-redux";
+import appStore from "./Redux/appStore";
 
 
 function AppLayout() {
   const isOnline = useOnlineStatus();
 
   return (
-    <div className="font-lato">
-      {isOnline ? <>
-        <Header />
-        <main className="mx-5" >
-          <Outlet />
-        </main>
-        <Footer />
-      </> : <NetworkMonitor />}
-    </div>
+    <Provider store={appStore}>
+      <div className="font-lato">
+        {isOnline ? <>
+          <Header />
+          <main className="mx-5" >
+            <Outlet />
+          </main>
+          <Footer />
+        </> : <NetworkMonitor />}
+      </div>
+    </Provider>
   );
 }
 
